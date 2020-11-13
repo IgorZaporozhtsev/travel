5fa963a065349c53780ec5fdb318ed44d308974f
zeecoder <igorzaporogtsev@gmail.com>
2020-11-13 11:44:01 +0200

    initial commit

---
+++ src/test/java/com/zeecoder/travel/TravelQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeecoder.travel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelQuestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
